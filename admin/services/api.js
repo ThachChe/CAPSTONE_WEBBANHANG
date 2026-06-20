8aac30ce53eb5f086395655f97397398a5a5f5fd
vanson2510 <vanson2510@gmail.com>
2026-06-20 20:59:40 +0700

    them tinh nang them san pham

--- admin/services/api.js
+++ admin/services/api.js
@@ -1,23 +1,33 @@
 class Services {
-    getListProductApi(){
-        const url = "https://6a183c611878294b597ca1d6.mockapi.io/api/WebBanHang";
+  getListProductApi() {
+    const url = "https://6a183c611878294b597ca1d6.mockapi.io/api/WebBanHang";
 
     const promise = axios({
-        url: url,
-        method: "GET",
+      url: url,
+      method: "GET",
     });
     return promise;
-    }
+  }
 
-    deleteProductApi(id){
-        const url = `https://6a183c611878294b597ca1d6.mockapi.io/api/WebBanHang/${id}`;
-        const promise = axios({
-            url: url,
-            method: "DELETE"
-        })
+  deleteProductApi(id) {
+    const url = `https://6a183c611878294b597ca1d6.mockapi.io/api/WebBanHang/${id}`;
+    const promise = axios({
+      url: url,
+      method: "DELETE",
+    });
 
-        return promise;
-    }
+    return promise;
+  }
+
+  addProductApi(product) {
+    const url = "https://6a183c611878294b597ca1d6.mockapi.io/api/WebBanHang";
+    const promise = axios({
+      url: url,
+      method: "POST",
+      data: product,
+    });
+    return promise;
+  }
 }
 
 export default Services;
