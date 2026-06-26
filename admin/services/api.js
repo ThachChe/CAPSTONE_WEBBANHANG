0f38a469e4dfc1feb0787664a83620fd2b6ce904
vanson2510 <vanson2510@gmail.com>
2026-06-26 21:53:21 +0700

    them tinh nang sua va cap nhat

--- admin/services/api.js
+++ admin/services/api.js
@@ -28,6 +28,25 @@ class Services {
     });
     return promise;
   }
+
+  getProductByIdApi(id) {
+    const url = `https://6a183c611878294b597ca1d6.mockapi.io/api/WebBanHang/${id}`;
+    const promise = axios({
+      url: url,
+      method: "GET",
+    });
+    return promise;
+  }
+
+  updateProductByIdApi(product) {
+    const url = `https://6a183c611878294b597ca1d6.mockapi.io/api/WebBanHang/${product.id}`;
+    const promise = axios({
+      url: url,
+      method: "PUT",
+      data: product,
+    });
+    return promise;
+  }
 }
 
 export default Services;
